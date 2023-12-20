7f489649d8df3e614a44d9c12e23e7645afc7c15
PieNczW <farrellaydin04@gmail.com>
2023-12-20 22:42:34 +0700

    extract_widget

---
+++ extract_widget/lib/main.dart
@@ -0,0 +1,52 @@
+
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var faker = new Faker();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("extract widget"),
+        ),
+        body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index){
+            return  ChatItem(
+              imageUrl: "https://picsum.photos/id/1/200/300",
+              title: faker.person.name(),
+              subtitle: faker.lorem.sentence(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  const ChatItem({this.imageUrl, this.title, this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text("10:00 pm"),
+    );
+  }
+}
